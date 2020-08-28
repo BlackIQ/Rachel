20890d3af70e67754718b71065a7e0a7b848d809
عرفان صابری | Erfan Saberi <55283540+erfansaberi@users.noreply.github.com>
2020-08-28 12:48:46 +0430

    Create chatDictionary.py

---
+++ chatDictionary.py
@@ -0,0 +1,9 @@
+chatDict = {
+        'you ok?':'yeah im ok',
+        'i love you':'I love you to honey :)))  ♥',
+        'i wanna tell something to you':'im all ears',
+        'i want to tell you something':'im listening',
+        'im not good today':'im so sorry to hear that',
+        'im so good today':'im so proud :)',
+        'how you feeling today?':'im feeling good',
+    }
