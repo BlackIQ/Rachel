c9e76eb70455d41d717f81d3d7c3a4c92ec402e7
عرفان صابری | Erfan Saberi <55283540+erfansaberi@users.noreply.github.com>
2020-09-02 23:04:52 +0430

    Update chatDictionary.py

--- chatDictionary.py
+++ chatDictionary.py
@@ -1,10 +1,11 @@
 chatDict = {
         'you ok?':'yeah im ok',
         'i love you':'I love you to honey :)))  ♥',
-        'i wanna tell something to you':'im all ears',
+        'i wanna tell something to you':['im all ears','tell me everything you want','im listening'],
         'i want to tell you something':'im listening',
         'im not good today':'im so sorry to hear that',
         'im so good today':'im so proud :)',
-        'how you feeling today?':'im feeling good',
-        'what is your favorite movie?':'i love Matrix & Her'
+        'how you feeling today?':['im feeling good','im good :)'],
+        'what is your favorite movie?':'i love Matrix & Her',
+        'what is your favorite anime?':['i can\'t choice just one','i love Sao','Sword art online, Tokyo ghoul, Another, Darwins game. they are too much but i like these four more than others'],
     }
