f53275567d27f401b7a6d3a50857c47279c23ce6
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-19 13:55:36 +0430

    Add comments

--- Rachel/execute.c
+++ Rachel/execute.c
@@ -1,3 +1,15 @@
+/*
+    Rachel
+
+    Written by -> Amirhossein Mohammadi & Erfan Saberi
+    github.com/BlackIQ
+    github.com/erfansaberi
+
+    Powered by PyAbr
+    PyAbr by Mani Jamali
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
