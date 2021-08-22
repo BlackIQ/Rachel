5930cdb5eae76745b9f521a10b76108c5e6bad12
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-22 10:23:53 +0430

    Get arg in main Rachel file

--- Rachel/rachel.py
+++ Rachel/rachel.py
@@ -11,6 +11,17 @@
 """
 
 import RachelCore
+import sys
+
+try:
+    if sys.argv[1] == "--update":
+        RachelCore.software.update()
+    if sys.argv[1] == "--uninstall":
+        RachelCore.software.uninstall()
+    if sys.argv[1] == "version":
+        print(RachelCore.software.version)
+except:
+    pass
 
 while True:
     # Get input
